843fb2978776f68ba3a84ea69ef688592270acb8
miguel <miguel.malini5963@gmail.com>
2023-11-06 14:42:11 -0300

    Novo Projeto

---
+++ src/main/java/com/example/origami/OrigamiApplication.java
@@ -0,0 +1,13 @@
+package com.example.origami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrigamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrigamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/origami/OrigamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.origami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrigamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
